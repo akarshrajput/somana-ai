ab554a6a982ad1c5a9343100f54042237b21b4ef
Akarsh Rajput <112371948+akarshrajput@users.noreply.github.com>
2025-03-06 15:10:37 +0530

    Update route.js

--- app/ai/route.js
+++ app/ai/route.js
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { NextResponse } from "next/server";
 
 // Initialize the generative AI with the API key from environment variables
-const genAI = new GoogleGenerativeAI("AIzaSyDBqoW36LDdmrGoOcjUUtfhR0-9GAnaxrA");
+const genAI = new GoogleGenerativeAI("AIzaSyANNY_3g8hk11Su4XiUv3BsYc3E_sYd3UU");
 
 export async function POST(req) {
   try {
